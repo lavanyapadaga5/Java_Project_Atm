47e5ee62942c8cf788ad8c7382a07a0796345061
lavanyapadaga5 <104807917+lavanyapadaga5@users.noreply.github.com>
2025-04-29 09:14:46 +0530

    Create ATMInterface.java

---
+++ ATMInterface.java
@@ -0,0 +1,10 @@
+package ATM;
+
+public interface ATMInterface {
+	public void Viewbalance();
+	public void Withdrawmount(double Withdrwamount);
+	public void Depositmount(double Depositamount);
+	public void ViewMiniStatement();
+	
+
+}
